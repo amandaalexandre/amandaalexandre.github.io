dde6de595f1987acb03a3a0d1a3fbb67e5bdc813
Amanda Alexandre <amanda.alexandre@outlook.com>
2022-02-18 17:03:58 -0300

    XMLHttpRequest successful

--- apod-nasa/photos.js
+++ apod-nasa/photos.js
@@ -1,20 +1,38 @@
 $(function() {
 
-    $("button").click(function() {
-        // $.get("https://api.nasa.gov/planetary/apod?api_key=6917575c-845c-41f2-a0cd-fd3d81b98144", mostraFoto())
-        // Tentando com .ajax
-        $.ajax({url: "https://api.nasa.gov/planetary/apod?api_key=6917575c-845c-41f2-a0cd-fd3d81b98144",
-        success: mostraFoto,
-        error: fotoDefault
-    })
-    })
-    
-    function mostraFoto() {
-        console.log("mostra foto")
+    let url = "https://api.nasa.gov/planetary/apod?api_key=NhimD9skbjdhA1Vaqf68FUj1HzIyuHAMAhy5yEtE";
+    var pedido = new XMLHttpRequest();
+    pedido.open("GET", url);
+    pedido.send();
+
+    pedido.onload = function() {
+        if(this.status == 200) {
+            const objDados = JSON.parse(this.responseText);
+            mostraFigura(objDados);
+        } else {
+            console("Requisição mal-sucedida!")
+        }
     }
 
-    function fotoDefault() {
-        console.log("deu erro")
+    function mostraFigura(objDados) {
+        let urlImagem = objDados.url;
+        let dataImagem = objDados.date;
+        let descricao = objDados.explanation;
+
+        $(".img-nasa").html(
+            `<img src="${urlImagem}">
+     
+            <p>${dataImagem}</p>
+            <p>${descricao}</p>
+            `
+        )
     }
+
+    $("button").click(function() {
+        let novaData = $("#dataPedido").val();
+        let novaUrl = `https://api.nasa.gov/planetary/apod?api_key=NhimD9skbjdhA1Vaqf68FUj1HzIyuHAMAhy5yEtE&date=${novaData}`
+        pedido.open("GET", novaUrl);
+        pedido.send();
+    }); 
 
 })
