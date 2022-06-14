2d94068423f16a9c87ed1dce4ad0411b0c70877a
Amanda Alexandre <amanda.alexandre@outlook.com>
2022-06-13 23:23:08 -0300

    feat: navbar

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,15 +1,25 @@
 import React from 'react'
 import '../assets/styles/header.css'
+
+// importing images
 import Logo from '../assets/images/logo.png'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faBars } from '@fortawesome/free-solid-svg-icons'
 
 export default function Header() {
 
     return (
         <nav className='nav-maior'>
-             <Logo />
+             <img src={Logo} className='logo'></img>
             <span className='nav-menu'>
-
+                <span className='links-menu'>
+                    github linkedin skills projects contact
+                </span>
+                
+                {/* Add menu link in the bars icon */}
+                <FontAwesomeIcon icon={faBars} className='menu-icon' />
             </span>
+           
         </nav>
     )
 }
