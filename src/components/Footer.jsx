cdd9eaf2c83095ea6a55e9227abfa876bb2dd285
Amanda Alexandre <amanda.alexandre@outlook.com>
2022-06-16 03:47:46 -0300

    feat: footer

---
+++ src/components/Footer.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+import '../assets/styles/footer.css'
+
+export default function Footer() {
+
+    return (
+        <footer>
+            <section className='follow-me'>
+                <div><h2>follow</h2></div>
+                <div><h2>me</h2></div>
+                <div>
+                    <a href="">Git</a>
+                    <a href="">LinkedIn</a>
+                </div>
+            </section>
+            <section className='disclaimer'>asdas</section>
+            <section className='bar'>asdas</section>
+
+        </footer>
+    )
+}
