51b140f6bb38beb001c7cd45a4d44e07c0e2d089
Amanda Alexandre <amanda.alexandre@outlook.com>
2022-06-16 01:41:08 -0300

    refactor: organizing projects pages

--- src/portfolioData.js
+++ src/portfolioData.js
@@ -3,12 +3,71 @@ export const portfolioData = [{
         "name": "NASA's Image of The Day",
         "live": "https://amandaalexandre.github.io/apod-nasa/",
         "repo": "https://github.com/amandaalexandre/apod-nasa",
-        "thumbnail": "../assets/thumbs/apod-nasa.png",
+        "thumbnail": "./assets/thumbs/apod-nasa.png",
         "stack": "API requests, CSS, HTML"
 },
 {
         "id": 2,
         "name": "Chocolatteria Ice Cream Landing Page",
-        ""
-}
+        "live": "https://amandaalexandre.github.io/chocolatteria/",
+        "repo": "https://github.com/amandaalexandre/chocolatteria",
+        "thumbnail": "./assets/thumbs/chocolatteria-ice-cream.png",
+        "stack": "HTML, CSS"
+},
+{
+        "id": 3,
+        "name": "Caesar's Cypher + Base 64 converter",
+        "live": "https://amandaalexandre.github.io/codex-romano/",
+        "repo": "https://github.com/amandaalexandre/codex-romano",
+        "thumbnail": "./assets/thumbs/caesars-cypher.png",
+        "stack": "Vanilla JavaScript, HTML, CSS"
+},
+{
+        "id": 4,
+        "name": "Consulting Company Landing Page",
+        "live": "https://amandaalexandre.github.io/consulting-html-css-landing-page/",
+        "repo": "https://github.com/amandaalexandre/consulting-html-css-landing-page",
+        "thumbnail": "./assets/thumbs/consulting-landing-page.png",
+        "stack": "Vanilla JavaScript, HTML, CSS"
+},
+{
+        "id": 5,
+        "name": "Marketing Company Landing Page",
+        "live": "https://amandaalexandre.github.io/marketing-landing-page/",
+        "repo": "https://github.com/amandaalexandre/marketing-landing-page",
+        "thumbnail": "./assets/thumbs/marketing-landing-page.png",
+        "stack": "Vanilla JavaScript, HTML, CSS"
+},
+{
+        "id": 6,
+        "name": "Netflix Clone with Bootstrap",
+        "live": "https://amandaalexandre.github.io/Techflix/",
+        "repo": "https://github.com/amandaalexandre/Techflix",
+        "thumbnail": "./assets/thumbs/techflix.png",
+        "stack": "Vanilla JavaScript, HTML, CSS"
+},
+{
+        "id": 7,
+        "name": "Steam Store Market Evaluation",
+        "live": "",
+        "repo": "https://github.com/amandaalexandre/dados-steam",
+        "thumbnail": "./assets/thumbs/techflix.png",
+        "stack": "Vanilla JavaScript, HTML, CSS"
+},
+{
+        "id": 8,
+        "name": "Bookstore API",
+        "live": "",
+        "repo": "https://github.com/camimonteiro/Project_API_Bookstore",
+        "thumbnail": "./assets/thumbs/api.png",
+        "stack": "Node.js, Insomnia, REST APIs"
+},
+{
+        "id": 9,
+        "name": "Bookstore React E-commerce Website",
+        "live": "http://livraria-macabeia-m5.vercel.app/",
+        "repo": "https://github.com/DaviFernandesSRN/Livraria-Macabeia--M5",
+        "thumbnail": "./assets/thumbs/bookstore-react-store.png",
+        "stack": "React, REST APIs"
+},
 ]
