d7adf89f4168f92bab6e427a908614b16225d1a0
Amanda Alexandre <amanda.alexandre@outlook.com>
2022-06-16 04:00:21 -0300

    style: footer layout

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,19 +1,27 @@
 import React from 'react'
 import '../assets/styles/footer.css'
 
+// importing icons
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faGithub, faLinkedinIn } from '@fortawesome/free-brands-svg-icons'
+
 export default function Footer() {
 
     return (
         <footer>
             <section className='follow-me'>
-                <div><h2>follow</h2></div>
-                <div><h2>me</h2></div>
+                <h2 className='follow'>follow</h2>
+                <h2 className='me'>me</h2>
                 <div>
-                    <a href="">Git</a>
+                    {/* <a href="">
+                        <FontAwesomeIcon icon={faGithub} />
+                    </a> */}
                     <a href="">LinkedIn</a>
                 </div>
             </section>
-            <section className='disclaimer'>asdas</section>
+            <section className='disclaimer'>
+                This is an original template created by me. If you decide to use it for your own portfolio, please provide credits to my Github page.
+            </section>
             <section className='bar'>asdas</section>
 
         </footer>
